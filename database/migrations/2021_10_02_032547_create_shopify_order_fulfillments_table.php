ace9c1aa11ef88fc55b5603541ba0348df788477
Nicolas de Mayo Bettelli <niketedm@gmail.com>
2021-11-19 17:22:41 -0300

    cambio nombre de funciones de migration

--- database/migrations/2021_10_02_032547_create_shopify_order_fulfillments_table.php
+++ database/migrations/2021_10_02_032547_create_shopify_order_fulfillments_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateShopifyOrderFulfillmentsTable extends Migration
+class CreateShopifyOrderFulfillmentsTable2 extends Migration
 {
     /**
      * Run the migrations.
